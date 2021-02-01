e3642500c783cd27376ac5fec65409a439bc6444
ondesly <dmitriytorkhov@gmail.com>
2021-02-01 13:29:14 +0300

    try/catch clause is removed from crud test

--- tests/test_crud.cpp
+++ tests/test_crud.cpp
@@ -23,54 +23,49 @@ int main() {
     lm::val update_value{};
     lm::val delete_value{};
 
-    try {
-        lm::db db(
-                {"./db.db", lm::flag::env::no_subdir},
-                {"test", lm::flag::dbi::create});
+    lm::db db(
+            {"./db.db", lm::flag::env::no_subdir},
+            {"test", lm::flag::dbi::create});
 
-        // Create
+    // Create
 
-        {
-            lm::transaction txn(db);
-            txn.put({key.data(), key.size()}, {value_1.data(), value_1.size()});
-        }
+    {
+        lm::transaction txn(db);
+        txn.put({key.data(), key.size()}, {value_1.data(), value_1.size()});
+    }
 
-        // Read
+    // Read
 
-        {
-            lm::transaction txn(db, lm::flag::transaction::rd_only);
-            create_value = txn.get({key.data(), key.size()});
-        }
+    {
+        lm::transaction txn(db, lm::flag::transaction::rd_only);
+        create_value = txn.get({key.data(), key.size()});
+    }
 
-        // Update
+    // Update
 
-        {
-            lm::transaction txn(db);
-            txn.put({key.data(), key.size()}, {value_2.data(), value_2.size()});
-        }
+    {
+        lm::transaction txn(db);
+        txn.put({key.data(), key.size()}, {value_2.data(), value_2.size()});
+    }
 
-        {
-            lm::transaction txn(db, lm::flag::transaction::rd_only);
-            update_value = txn.get({key.data(), key.size()});
-        }
+    {
+        lm::transaction txn(db, lm::flag::transaction::rd_only);
+        update_value = txn.get({key.data(), key.size()});
+    }
 
-        // Delete
+    // Delete
 
-        {
-            lm::transaction txn(db);
-            txn.del({key.data(), key.size()});
-        }
+    {
+        lm::transaction txn(db);
+        txn.del({key.data(), key.size()});
+    }
 
-        {
-            lm::transaction txn(db, lm::flag::transaction::rd_only);
-            delete_value = txn.get({key.data(), key.size()});
-        }
-
-    } catch (const std::exception &e) {
-        std::cerr << e.what() << std::endl;
+    {
+        lm::transaction txn(db, lm::flag::transaction::rd_only);
+        delete_value = txn.get({key.data(), key.size()});
     }
 
     return (std::vector<uint8_t>(create_value.data, create_value.data + create_value.size) == value_1 &&
             std::vector<uint8_t>(update_value.data, update_value.data + update_value.size) == value_2 &&
-           std::vector<uint8_t>(delete_value.data, delete_value.data + delete_value.size).empty()) ? 0 : 1;
+            std::vector<uint8_t>(delete_value.data, delete_value.data + delete_value.size).empty()) ? 0 : 1;
 }
